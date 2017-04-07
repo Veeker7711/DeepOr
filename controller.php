6beaca9a365ddef6532401018392ad2fdd109f99
root <root@veeker>
2017-04-07 04:29:54 -0400

    set

---
+++ controller.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gendar_t
+ * Date: 07/04/2017
+ * Time: 09:01
+ */
+
+require_once "PHPExcel-1.8/Classes/PHPExcel.php";
+
+class Controller
+{
+    private $arr_value;
+    private $file_name;
+
+
+    /* Must receive an excel file to convert it in the array $arr_value */
+    public function __construct($xlsx)
+    {
+        $this->file_name = $xlsx;
+        $file = $xlsx;
+
+        $pathInfo = pathinfo($file);
+        $type = $pathInfo['extension'] == 'xlsx' ? 'Excel2007' : 'Excel5';
+
+        $objReader = PHPExcel_IOFactory::createReader($type);
+        $objPHPExcel = $objReader->load($file);
+
+        foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
+            $this->arr_value[$worksheet->getTitle()] = $worksheet->toArray();
+        }
+        return ($this->arr_value);
+    }
+
+}
+
+$tmp = new Controller("data.xlsx");
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gendar_t
+ * Date: 07/04/2017
+ * Time: 09:48
+ */
+?>
+    <!DOCTYPE html>
+    <?php require "controller.php"; ?>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+<?php
+var_dump(new Controller("data.xlsx"))
+?>
+</body >
+</html >
